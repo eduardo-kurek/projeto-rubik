207afc97bdc9c7d7d5183e4987ab66dee5d1c144
eduardo-kurek <eduardokurek2003@gmail.com>
2023-10-05 13:33:51 -0300

    Implementando as camadas

---
+++ rubik/classes/auxiliares/Layer.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdint>
+
+class Layer{
+    uint8_t si;
+    uint8_t ei;
+    uint8_t sj;
+    uint8_t ej;
+
+public:
+    Layer(uint8_t si, uint8_t ei, uint8_t sj, uint8_t ej);
+};
+
+extern const Layer LAYER_U;
+extern const Layer LAYER_UA;
+extern const Layer LAYER_D;
+extern const Layer LAYER_DA;
+extern const Layer LAYER_R;
+extern const Layer LAYER_RA;
+extern const Layer LAYER_L;
+extern const Layer LAYER_LA;
+extern const Layer LAYER_M;
+extern const Layer LAYER_MA;
+extern const Layer LAYER_E;
+extern const Layer LAYER_EA;
