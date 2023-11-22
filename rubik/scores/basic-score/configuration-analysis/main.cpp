fbbbc7a0c2314c0203007df38b690c623936664d
Eduardo Kurek <eduardokurek2003@gmail.com>
2023-11-22 15:45:48 -0300

    Análise do BasicScore agora retorna um float entre 0 e 2000, quanto maior, melhor

--- rubik/scores/basic-score/configuration-analysis/main.cpp
+++ rubik/scores/basic-score/configuration-analysis/main.cpp
@@ -2,11 +2,14 @@
 #include <fstream>
 #include <filesystem>
 #include <utility>
+#include <cmath>
 #include "../BasicScore.h"
 #include "../../PontuationTable.h"
 #ifdef _WIN32
 #include <windows.h>
 #endif
+
+// #define DEBUG
 
 bool validate_files(std::filesystem::path path, std::vector<std::string> fileNames){
     for (const auto& fileName : fileNames) {
@@ -20,6 +23,8 @@ bool validate_files(std::filesystem::path path, std::vector<std::string> fileNam
 }
 
 PontuationTable* mount_pontuation_table(char* argv[]){
+    // 3 2 2 1 0 3 2 1 0
+
     // MONTANDO TABELA DE PONTUAÇÃO ATRAVÉS DOS VALORES INFORMADOS
     std::vector<float> cornerPontuation;
     std::vector<float> edgePontuations;
@@ -28,6 +33,12 @@ PontuationTable* mount_pontuation_table(char* argv[]){
         if(i < 6) cornerPontuation.push_back(std::stof(argv[i]));
         else edgePontuations.push_back(std::stof(argv[i]));
     }
+
+    // VERIFICANDO SE O PRIMEIRO VALOR DE CADA VETOR É O MAIOR DO VETOR INTEIRO
+    if(*std::max_element(cornerPontuation.begin(), cornerPontuation.end()) != cornerPontuation[0])
+        return nullptr;
+    if(*std::max_element(edgePontuations.begin(), edgePontuations.end()) != edgePontuations[0])
+        return nullptr;
 
     return new PontuationTable(cornerPontuation, edgePontuations);
 }
@@ -60,16 +71,75 @@ std::vector<float> files_to_pontuation(std::vector<std::string> fileNames, std::
     return pontuations;
 }
 
-void print_pontuations(std::vector<float> pontuations){
-    std::cout << "{";
+float analize_pontuations(std::vector<float> pontuations, std::vector<float> expectedPontuations, float maxDiff, int lowerTreshold){
     
-    for(int i = 0; i < pontuations.size(); i++){
-        std::cout << "\"" << i+1 << "\": ";
-        std::cout << std::fixed << std::setprecision(2) << pontuations[i];
-        std::cout << ",";
+    if(pontuations.size() != 20){
+        std::cerr << "Tamanho do vetor de pontuações inválido.";
+        throw new std::invalid_argument("Tamanho do vetor de pontuações inválido.");
     }
 
-    std::cout << "\b}";
+    if(expectedPontuations.size() != 20){
+        std::cerr << "Tamanho do vetor de pontuações esperadas inválido.\n";
+        throw new std::invalid_argument("Tamanho do vetor de pontuações esperadas inválido.");
+    }
+
+    if(lowerTreshold > 20 || lowerTreshold <= 0){
+        std::cerr << "Limite inferior deve estar entre 1 e 20\n";
+        throw new std::invalid_argument("Limite inferior deve estar entre 1 e 20.");
+    }
+    
+    float sum = 0;
+
+    // LOOP ATÉ OS 16 PRIMEIROS VALORES
+    for(int i = 0; i < lowerTreshold-1; i++){
+        float diff = std::abs(pontuations[i] - expectedPontuations[i]);
+
+        #ifdef DEBUG
+            std::cout << "Diferença do " << i+1 << "º arquivo: " << pontuations[i] << " - " <<
+            expectedPontuations[i] << " = " << diff << ", portanto, soma " << 100 - diff << ".\n";
+        #endif
+        
+        // SE A DIFERENÇA FOR MAIOR QUE O PERMITIDO, CONFIGURAÇÃO INVÁLIDA
+        if(diff > maxDiff) return 0;
+        sum += 100 - diff;
+    }
+
+    // LOOP PARA O RESTANTE DOS VALORES
+    for(int i = lowerTreshold-1; i < 20; i++){
+
+
+        float diff = std::abs(pontuations[i] - expectedPontuations[i]);
+
+        #ifdef DEBUG
+            std::cout << "Diferença do " << i+1 << "º arquivo: " << pontuations[i] << " - " <<
+            expectedPontuations[i] << " = " << diff << ", portanto, soma " << 100 - diff << ".\n";
+        #endif
+
+        if(pontuations[i] > expectedPontuations[lowerTreshold-1] + maxDiff){
+            #ifdef DEBUG
+                std::cout << "Pontuação do " << i+1 << "º arquivo é maior que a do " << lowerTreshold << "º arquivo. (" <<
+                pontuations[i] << " <= " << pontuations[lowerTreshold-1] << ")\n";
+            #endif
+            
+            return 0;
+        }
+
+        sum += 100 - diff;
+    }
+
+    return sum;
+}
+
+void print_pontuations(std::vector<float> pontuations){
+    std::cout << "{\n";
+    
+    for(int i = 0; i < pontuations.size(); i++){
+        std::cout << "\t\"" << i+1 << "\": ";
+        std::cout << std::fixed << std::setprecision(2) << pontuations[i];
+        std::cout << ",\n";
+    }
+
+    std::cout << "\b}\n";
 }
 
 int main(int argc, char* argv[]){
@@ -97,13 +167,29 @@ int main(int argc, char* argv[]){
 
     // INSTÂNCIA DO SCORE
     PontuationTable* pontuationTable = mount_pontuation_table(argv);
+    if(pontuationTable == nullptr){
+        std::cerr << "O primeiro valor de cada vetor deve ser o maior.\n";
+        return 3;
+    }
+
     BasicScore* score = new BasicScore(pontuationTable);
 
     // ARMAZENANDO A MÉDIA DE PONTUAÇÃO DE CADA ARQUIVO
     std::vector<float> pontuations = files_to_pontuation(fileNames, path, score);
 
     // IMPRIMINDO RESULTADOS NO FORMATO JSON
+    #ifdef DEBUG
     print_pontuations(pontuations);
+    #endif
+
+    // ANALISANDO PONTUAÇÕES OBTIDAS
+    std::vector<float> expectedPontuations = {
+        95, 90, 85, 80, 75, 70, 65, 60, 55, 50, 
+        45, 40, 35, 30, 27, 25 ,20, 15, 10, 8
+    };
+    float result = analize_pontuations(pontuations, expectedPontuations, 40, 14);
+
+    std::cout << result << "\n";
 
     return 0;
 }
