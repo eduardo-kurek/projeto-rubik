4d6b54084cccc59200d221ae50bbf2be2cfe87a5
Eduardo Kurek <eduardokurek2003@gmail.com>
2024-02-12 14:55:52 -0300

    Implementando classe para analise

--- rubik/scores/basic-score/configuration-analysis/main.cpp
+++ rubik/scores/basic-score/configuration-analysis/main.cpp
@@ -10,153 +10,82 @@
 #include <windows.h>
 #endif
 
-// #define DEBUG
+#define DEBUG
 
-bool validate_files(std::filesystem::path path, std::vector<std::string> fileNames){
-    for (const auto& fileName : fileNames) {
-        std::filesystem::path filePath = path / fileName;
-        if (!std::filesystem::exists(filePath)){
-            std::cerr << fileName << " não existe dentro do caminho fornecido.\n";
+using namespace std;
+
+class Analyzer{
+
+    private:
+
+    filesystem::path scramblePath = "../../../../scrambles/";
+    vector<string> fileNames;
+    vector<string> config;
+    vector<float> expectedPontuations = {95,90,85,80,75,70,65,60,55,50,45,40,35,30,25,20,15,10,5,0};
+    PontuationTable* table = nullptr;
+    vector<float> result;
+
+    public:
+
+    Analyzer(filesystem::path scramblePath, vector<string> fileNames, vector<string> config){
+        this->scramblePath = scramblePath;
+        this->fileNames = fileNames;
+        this->config = config;
+    }
+
+    bool validate_files(){
+        for (const auto& fileName : this->fileNames) {
+            filesystem::path filePath = this->scramblePath / fileName;
+            if (!filesystem::exists(filePath)){
+                cerr << fileName << " não existe dentro do caminho fornecido.\n";
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool mount_table(){
+        // MONTANDO TABELA DE PONTUAÇÃO ATRAVÉS DOS VALORES INFORMADOS
+        std::vector<float> cornerPontuation;
+        std::vector<float> edgePontuations;
+
+        for(int i = 1; i < 10; i++){
+            if(i < 6) cornerPontuation.push_back(stof(this->config[i]));
+            else edgePontuations.push_back(stof(this->config[i]));
+        }
+
+        // VERIFICANDO SE O PRIMEIRO VALOR DE CADA VETOR É O MAIOR DO VETOR INTEIRO
+        if(*std::max_element(cornerPontuation.begin(), cornerPontuation.end()) != cornerPontuation[0])
             return false;
+        if(*std::max_element(edgePontuations.begin(), edgePontuations.end()) != edgePontuations[0])
+            return false;
+
+        this->table = new PontuationTable(cornerPontuation, edgePontuations);
+        return true;
+    }
+
+    void print_pontuations(){
+        if(this->result.size() != 20) return;
+
+        cout << "{\n";
+
+        for(int i = 0; i < this->result.size(); i++){
+            cout << "\t\"" << i+1 << "\": ";
+            cout << fixed << setprecision(2) << this->result[i];
+            cout << ",\n";
         }
-    }
-    return true;
-}
 
-PontuationTable* mount_pontuation_table(char* argv[]){
-    // 3 2 2 1 0 3 2 1 0
-
-    // MONTANDO TABELA DE PONTUAÇÃO ATRAVÉS DOS VALORES INFORMADOS
-    std::vector<float> cornerPontuation;
-    std::vector<float> edgePontuations;
-
-    for(int i = 1; i < 10; i++){
-        if(i < 6) cornerPontuation.push_back(std::stof(argv[i]));
-        else edgePontuations.push_back(std::stof(argv[i]));
+        cout << "\b}\n";
     }
 
-    // VERIFICANDO SE O PRIMEIRO VALOR DE CADA VETOR É O MAIOR DO VETOR INTEIRO
-    if(*std::max_element(cornerPontuation.begin(), cornerPontuation.end()) != cornerPontuation[0])
-        return nullptr;
-    if(*std::max_element(edgePontuations.begin(), edgePontuations.end()) != edgePontuations[0])
-        return nullptr;
+    float analyse(){
+        if(this->table == nullptr) 
+            throw runtime_error("É preciso validar a tabela de pontuação antes de analisar.");
 
-    return new PontuationTable(cornerPontuation, edgePontuations);
-}
-
-float average_pontuation_file(std::filesystem::path filePath, BasicScore* score){
-    std::ifstream file(filePath);
-    std::string scramble;
-    float sum = 0;
-    int count = 0;
-
-    while (std::getline(file, scramble)){
-        std::vector<const Move*> moves = Move::stringToMoves(scramble);
-        Rubik rubik;
-        rubik.move(moves);
-
-        sum += score->calculateNormalized(rubik);
-        count++;
+        
     }
 
-    return sum / count;
-}
-
-std::vector<float> files_to_pontuation(std::vector<std::string> fileNames, std::filesystem::path rootPath, BasicScore* score){
-    std::vector<float> pontuations(fileNames.size());
-
-    #pragma omp parallel for
-    for(int i = 0; i < fileNames.size(); i++){
-        pontuations[i] = average_pontuation_file(rootPath / fileNames[i], score);
-    }
-
-    return pontuations;
-}
-
-float analize_pontuations(std::vector<float> pontuations, std::vector<float> expectedPontuations, const float maxDiff, int lowerTreshold){
-
-    if(pontuations.size() != 20){
-        std::cerr << "Tamanho do vetor de pontuações inválido (" << pontuations.size() << ").\n";
-        throw new std::invalid_argument("Tamanho do vetor de pontuações inválido");
-    }
-
-    if(expectedPontuations.size() != 20){
-        std::cerr << "Tamanho do vetor de pontuações esperadas inválido.\n";
-        throw new std::invalid_argument("Tamanho do vetor de pontuações esperadas inválido.");
-    }
-
-    if(lowerTreshold > 20 || lowerTreshold <= 0){
-        std::cerr << "Limite inferior deve estar entre 1 e 20\n";
-        throw new std::invalid_argument("Limite inferior deve estar entre 1 e 20.");
-    }
-
-    float sum = 0;
-
-    // LOOP ATÉ OS 16 PRIMEIROS VALORES
-    #pragma omp parallel for reduction(+:sum)
-    for(int i = 0; i < lowerTreshold-1; i++){
-        float diff = std::abs(pontuations[i] - expectedPontuations[i]);
-
-        #ifdef DEBUG
-            #pragma omp critical
-            {
-                std::cout << "Diferença do " << i+1 << "º arquivo: " << pontuations[i] << " - " <<
-                expectedPontuations[i] << " = " << diff << ", portanto, soma " << 100 - diff << ".\n";
-            }
-        #endif
-
-        // SE A DIFERENÇA FOR MAIOR QUE O PERMITIDO, CONFIGURAÇÃO INVÁLIDA
-        if(diff > maxDiff){
-            #ifdef DEBUG
-                #pragma omp critical
-                {
-                    std::cout << "Diferença do " << i+1 << "º arquivo é maior que o permitido. (" << diff << " > " << maxDiff << ")\n";
-                }
-            #endif
-            sum += 50 - diff;
-        }
-        else sum += 100 - diff;
-    }
-
-    // LOOP PARA O RESTANTE DOS VALORES
-    #pragma omp parallel for reduction(+:sum)
-    for(int i = lowerTreshold-1; i < 20; i++){
-        float diff = std::abs(pontuations[i] - expectedPontuations[i]);
-
-        #ifdef DEBUG
-            #pragma omp critical
-            {
-                std::cout << "Diferença do " << i+1 << "º arquivo: " << pontuations[i] << " - " <<
-                expectedPontuations[i] << " = " << diff << ", portanto, soma " << 100 - diff << ".\n";
-            }
-        #endif
-
-        if(pontuations[i] > expectedPontuations[lowerTreshold-1] + maxDiff){
-            #ifdef DEBUG
-                #pragma omp critical
-                {
-                    std::cout << "Pontuação do " << i+1 << "º arquivo é maior que a do " << lowerTreshold << "º arquivo. (" <<
-                    pontuations[i] << " <= " << pontuations[lowerTreshold-1] << ")\n";
-                }
-            #endif
-            sum += 50 - diff;
-        }else sum += 100 - diff;
-    }
-
-    return sum;
-}
-
-void print_pontuations(std::vector<float> pontuations){
-    std::cout << "{\n";
-
-    for(int i = 0; i < pontuations.size(); i++){
-        std::cout << "\t\"" << i+1 << "\": ";
-        std::cout << std::fixed << std::setprecision(2) << pontuations[i];
-        std::cout << ",\n";
-    }
-
-    std::cout << "\b}\n";
-}
+};
 
 int main(int argc, char* argv[]){
     #ifdef _WIN32
@@ -169,43 +98,30 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
-    std::filesystem::path path = "../../../../scrambles/";
-    std::vector<std::string> fileNames = {
+    filesystem::path path = "../../../../scrambles/";
+    vector<string> fileNames = {
         "1move.scr", "2moves.scr", "3moves.scr", "4moves.scr", "5moves.scr",
         "6moves.scr", "7moves.scr", "8moves.scr", "9moves.scr", "10moves.scr",
         "11moves.scr", "12moves.scr", "13moves.scr", "14moves.scr", "15moves.scr",
         "16moves.scr", "17moves.scr", "18moves.scr", "19moves.scr", "20moves.scr"
     };
 
-    // VALIDANDO OS ARQUIVOS
-    if(!validate_files(path, fileNames))
-        return 2;
+    vector<string> args;
+    for(int i = 1; i < argc; i++)
+        args.push_back(argv[i]);
+    
 
-    // INSTÂNCIA DO SCORE
-    PontuationTable* pontuationTable = mount_pontuation_table(argv);
-    if(pontuationTable == nullptr){
-        //std::cerr << "O primeiro valor de cada vetor deve ser o maior.\n";
-        return 3;
-    }
+    Analyzer* analyzer = new Analyzer(path, fileNames, args);
 
-    BasicScore* score = new BasicScore(pontuationTable);
+    if(!analyzer->validate_files()) return 2;
+    if(!analyzer->mount_table()) return 3;
 
-    // ARMAZENANDO A MÉDIA DE PONTUAÇÃO DE CADA ARQUIVO
-    std::vector<float> pontuations = files_to_pontuation(fileNames, path, score);
 
-    // IMPRIMINDO RESULTADOS NO FORMATO JSON
     #ifdef DEBUG
-    print_pontuations(pontuations);
+    analyzer->print_pontuations();
     #endif
 
-    // ANALISANDO PONTUAÇÕES OBTIDAS
-    std::vector<float> expectedPontuations = {
-        95, 90, 85, 80, 75, 70, 65, 60, 55, 50,
-        45, 40, 35, 30, 27, 25 ,20, 15, 10, 8
-    };
-    float result = analize_pontuations(pontuations, expectedPontuations, 10, 14);
-
-    std::cout << result << "\n";
+    analyzer->analyse();
 
     return 0;
 }
