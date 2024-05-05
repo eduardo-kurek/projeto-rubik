f8c145163a663033c3fc187dcd297658fc140392
Eduardo Kurek <eduardokurek2003@gmail.com>
2024-05-05 17:10:17 -0300

    Criando função para gerar número entre um determinado limite

--- services/Random.cpp
+++ services/Random.cpp
@@ -4,4 +4,9 @@ namespace svc {
 
     std::mt19937_64 Random::MT(37u);
 
+    int Random::uniform_int_distribution(int min, int max){
+        std::uniform_int_distribution<int> dist(min, max);
+        return dist(MT);
+    }
+
 }
--- services/Random.h
+++ services/Random.h
@@ -6,6 +6,7 @@ namespace svc {
 
         public:
             static std::mt19937_64 MT;
+            static int uniform_int_distribution(int min, int max);
 
     };
 
