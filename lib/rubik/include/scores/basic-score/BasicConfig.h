80d0f914c7b8755ae423d033f922f48ab54dabc7
Eduardo Kurek <eduardokurek2003@gmail.com>
2024-09-19 15:26:23 -0300

    Alerta sobre valores negativos

--- lib/rubik/include/scores/basic-score/BasicConfig.h
+++ lib/rubik/include/scores/basic-score/BasicConfig.h
@@ -4,6 +4,18 @@
 #include <string>
 #include <array>
 
+/*
+* Valores negativos podem causar comportamento inesperado, como por exemplo:
+*
+* Config: corners = {1, -5, -9, -6}, edges = {-96, -78, 35, -6}, synergy = 0
+* Cubo com 10 movimentos testado:
+* Max Score: -1144
+* Score: -482
+* Score em porcentagem: 42.1329
+*
+* Um valor em teoria maior do Score, resulta em uma porcentagem menor...
+* O comportamento não é como o esperado, mas funciona mesmo com valores negativos
+*/
 class BasicConfig : public Config{
 
     using Array = std::array<float, 4>;
